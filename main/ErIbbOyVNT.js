30dc7b1c796a097aef26a3b56121b81aee5c68d6
AnbuReproduce <martin91604595@gmail.com>
2026-05-04 10:24:00 -0500

    Initial commit

---
+++ main/ErIbbOyVNT.js
@@ -0,0 +1,57 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+function uniqueArray(array) {
+  return [...new Set(array)];
+}
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+function debounce(func, delay) {
+  let timeoutId;
+  return function(...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function() {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function() {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+function isPalindrome(str) {
+  const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleaned === cleaned.split('').reverse().join('');
+}
+function factorial(n) {
+  if (n < 0) return undefined;
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
+function fibonacci(n) {
+  if (n < 0) return undefined;
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
